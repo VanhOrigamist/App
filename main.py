97ee2d78563108786dcb7bf255a2701f180b1dfd
Nguyễn Việt Anh <96413432+VanhOrigamist@users.noreply.github.com>
2024-09-07 23:39:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-# Import
-from kivymd.app import MDApp
-from kivy.lang import Builder
+# # Import
+# from kivymd.app import MDApp
+# from kivy.lang import Builder
 
-# Build app
+# # Build app
 
-class Test(MDApp):
+# class Test(MDApp):
+#     def build(self):
+#         self.title = 'Programming'
+#         return Builder.load_file('Main.kv')
+
+# # Run app
+# Test().run()
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+
     def build(self):
-        self.title = 'Programming'
-        return Builder.load_file('Main.kv')
-
-# Run app
-Test().run()
+        return Label(text='Hello world')
+MyApp().run()
